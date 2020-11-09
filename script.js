dbbb33d2280786e389c9ec94babb7d13ce0fff05
Josh Archer <jarcher@greenriver.edu>
2020-11-09 09:51:38 -0800

    Updated the example to include a white background to make it easier to identify elements.

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 
 
 window.onload = function() {
+    //select all three elements
     let elements = document.querySelectorAll(
         "div, section, p");
+
+    //add an event handler to all three that changes
+    //the background color when clicked!
     for (let i = 0; i < elements.length; i++)
     {
         let element = elements[i];
